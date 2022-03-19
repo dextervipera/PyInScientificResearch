cc545674da798a56267a6c420a94b556c1373d04
dextervipera <89567341+dextervipera@users.noreply.github.com>
2022-03-19 10:23:56 +0100

    Update lab02_ch02.py

--- lab02_ch02.py
+++ lab02_ch02.py
@@ -1,17 +1,23 @@
+# capitelize every first (word || line) letter
+
+## test string
 s0='xxx xxx\nyy yy\nzzz zzzzz'
+
+## simple solution
 chk = ''.join([s0[id].upper() if (s0[id-1] in [' ', '\n']) else s0.capitalize()[id] for id, _ in enumerate(s0)])
 print(chk)
 
-def lab02_ch(str, sub='',i=0):
+## simplier solution
+def lab02_ch(str, sub=''):
  if len(str)==1:
   return str.upper()+sub
  else:
   if str[-2] in (' ','\n'):
-    return lab02_ch(str[:-1], str[-1:].upper()+sub,i)
+    return lab02_ch(str[:-1], str[-1:].upper()+sub)
   else:
-    return lab02_ch(str[:-1], str[-1]+sub,i)
+    return lab02_ch(str[:-1], str[-1]+sub)
 
-
+## even simplier solution
 def lab021_ch(str, sub=''):
   return str.upper()+sub if len(str)==1 else lab02_ch(str[:-1], str[-1:].upper()+sub if str[-2] in (' ','\n') else str[-1:]+sub)
   
