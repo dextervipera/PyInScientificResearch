06005e0e8e8469a14906fe572555111a9d07f6d7
dextervipera <89567341+dextervipera@users.noreply.github.com>
2022-03-20 22:38:33 +0100

    spell fix

--- lab02_ch02.py
+++ lab02_ch02.py
@@ -4,6 +4,7 @@
 s0='xxx xxx\nyy yy\nzzz zzzzz'
 
 ## simple solution
+print('Simple solution')
 chk = ''.join([s0[id].upper() if (s0[id-1] in [' ', '\n']) else s0.capitalize()[id] for id, _ in enumerate(s0)])
 print(chk)
 
@@ -19,6 +20,7 @@ def lab02_ch(str, sub=''):
 
 ## even simplier solution
 def lab021_ch(str, sub=''):
-  return str.upper()+sub if len(str)==1 else lab021_ch(str[:-1], str[-1:].upper()+sub if str[-2] in (' ','\n') else str[-1:]+sub)
-  
+  return str.upper()+sub if len(str)==1 else lab021_ch(str[:-1], str[-1:].upper()+sub) if str[-2] in (' ','\n') else lab021_ch(str[:-1], str[-1]+sub)
+
+print('Even simplier')
 print(lab021_ch(s0))
